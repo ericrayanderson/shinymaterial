2a8ac3ff0c0c7a229f67be9e6cee2dc23fb2731b
ericrayanderson <eric.ray.anderson@gmail.com>
2017-04-29 18:59:56 -0400

    documentation

--- R/shiny-material-switch.R
+++ R/shiny-material-switch.R
@@ -25,13 +25,13 @@ material_switch <- function(input_id, label, off_label, on_label, initial_value 
         shiny::tags$head(
           shiny::tags$style(
             paste0(
-              '#', input_id, '_switch.switch label input[type=checkbox]:checked+.lever:after {
-              background-color: ', color, '!important;
+              "#", input_id, "_switch.switch label input[type=checkbox]:checked+.lever:after {
+              background-color: ", color, "!important;
               }
-              #', input_id, '_switch.switch label input[type=checkbox]:checked+.lever {
-              background-color: ', color, '!important;
+              #", input_id, "_switch.switch label input[type=checkbox]:checked+.lever {
+              background-color: ", color, "!important;
               }
-              '
+              "
             )
           )
         )
