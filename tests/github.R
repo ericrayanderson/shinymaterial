bebf7e4738fc68621c84ed115fedaaca5804b867
ericrayanderson <eric.ray.anderson@gmail.com>
2017-07-04 19:27:44 -0400

    updates

--- tests/github.R
+++ tests/github.R
@@ -1,12 +1,9 @@
 library(shinymaterial)
 
 ui <- material_page(
-  material_tabs(
-    tabs = c(
-      "tab" = "first_tab",
-      "tab" = "second_tab"
-    )
-  )
+  
 )
-server <- function(input, output) {}
+server <- function(input, output) {
+  
+}
 shinyApp(ui = ui, server = server)
