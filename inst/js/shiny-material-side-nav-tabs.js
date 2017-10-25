75366ac7802a2eb3e21f39df5455bc97766d1fba
ericrayanderson <eric.ray.anderson@gmail.com>
2017-10-24 21:04:25 -0400

    updates

--- inst/js/shiny-material-side-nav-tabs.js
+++ inst/js/shiny-material-side-nav-tabs.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
     //$('.shiny-material-side-nav-tab-content').hide();
     $('.shiny-material-side-nav-tab').first().addClass('active');
     var first_side_nav_tab_id = $('.shiny-material-side-nav-tab').first().attr("id");
-    console.log(first_side_nav_tab_id);
+    //console.log(first_side_nav_tab_id);
+    //var first_side_nav_tab_content_id = first_side_nav_tab_id.replace("_tab_id", "");
+    //$("'#" + first_side_nav_tab_content_id + "'").css('visibility', 'visible');
     $('.shiny-material-side-nav-tab-content').first().css('visibility', 'visible');
     //$('.shiny-material-side-nav-tabs').first().click();
 })
