b8e1ed2dda8306c4441a1cf6e7e774577dc6d8eb
ericrayanderson <eric.ray.anderson@gmail.com>
2017-11-20 19:40:46 -0500

    fix checkbox

--- inst/js/shiny-material-checkbox.js
+++ inst/js/shiny-material-checkbox.js
@@ -1,30 +1,22 @@
 $(document).ready(function () {
-  
-  function initShinyMaterialDatePicker(callback) {
-        $('.datepicker').pickadate({});
-        callback();
-    }
-
-    initShinyMaterialDatePicker(function () {
-      
-    var shinyMaterialDatePicker = new Shiny.InputBinding();
-    $.extend(shinyMaterialDatePicker, {
-        find: function (scope) {
-            return $(scope).find(".shiny-material-date-picker");
-        },
-        getValue: function (el) {
-            return $(el).val();
-        },
-        subscribe: function (el, callback) {
-            $(el).on("change.shiny-material-date-picker", function (e) {
-                callback();
-            });
-        },
-        unsubscribe: function (el) {
-            $(el).off(".shiny-material-date-picker");
-        }
-    });
-
-    Shiny.inputBindings.register(shinyMaterialDatePicker);
-    });
-});
+ 
+     var shinyMaterialCheckbox = new Shiny.InputBinding();
+     $.extend(shinyMaterialCheckbox, {
+         find: function (scope) {
+             return $(scope).find(".shiny-material-checkbox");
+         },
+         getValue: function (el) {
+             return $(el).val();
+         },
+         subscribe: function (el, callback) {
+             $(el).on("change.shiny-material-checkbox", function (e) {
+                 callback();
+             });
+         },
+         unsubscribe: function (el) {
+             $(el).off(".shiny-material-checkbox");
+         }
+     });
+ 
+     Shiny.inputBindings.register(shinyMaterialCheckbox);
+ });
