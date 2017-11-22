affe5c7d1edd1ae6b77a2a6c1c53e5abf510965a
ericrayanderson <eric.ray.anderson@gmail.com>
2017-11-22 16:25:57 -0500

    include fonts

--- R/shiny-material-page.R
+++ R/shiny-material-page.R
@@ -6,6 +6,7 @@
 #' @param nav_bar_color Color of the navigation bar. Leave blank for the default color. Visit \url{http://materializecss.com/color.html} for a list of available colors.
 #' @param background_color Page background color. Leave blank for the default color. Visit \url{http://materializecss.com/color.html} for a list of available colors.
 #' @param font_color String. The title font color. Leave blank for the default color. Visit \url{http://materializecss.com/color.html} for a list of available colors. \emph{Title color requires using word forms of colors (e.g. "deep-purple"). Also, lighten or darken effects do not work on title colors.}
+#' @param include_fonts Boolean. Should the material font files be included? (This will place the font sources in a directory 'www', at the same location as the app code.)
 #' @examples
 #' material_page(
 #'   title = "Example Title",
@@ -13,7 +14,29 @@
 #'   background_color = "blue lighten-4",
 #'   shiny::tags$h1("Page Content")
 #' )
-material_page <- function(..., title = "", nav_bar_color = NULL, background_color = "grey lighten-4", font_color = NULL){
+material_page <- function(..., title = "", nav_bar_color = NULL, background_color = "grey lighten-4", font_color = NULL, include_fonts = FALSE){
+  
+  if(include_fonts){
+    
+    dir_recursion <- c("www", "www/fonts", "www/fonts/roboto")
+    
+    for(dir.i in dir_recursion){
+      if(!dir.exists(dir.i)){
+        dir.create(dir.i)
+      }
+    }
+    
+    font_files <- list.files(system.file("www/fonts/roboto",
+                                         package = "shinymaterial"))
+    
+    for(font_file.i in font_files){
+      file.copy(from = system.file(paste0("www/fonts/roboto/", font_file.i),
+                                   package = "shinymaterial"),
+                to = paste0("www/fonts/roboto/", font_file.i),
+                overwrite = TRUE)
+    }
+  }
+  
   shiny::tags$html(
     # Head --------------------------------------------------------------------
     shiny::tags$head(
---
+++ tests/include_font_app/app.R
@@ -0,0 +1,14 @@
+library(shiny)
+library(shinymaterial)
+
+# Wrap shinymaterial apps in material_page
+ui <- material_page(
+  include_fonts = TRUE,
+  title = "Basic Page",
+  tags$h1("Page Content")
+)
+
+server <- function(input, output) {
+  
+}
+shinyApp(ui = ui, server = server)
