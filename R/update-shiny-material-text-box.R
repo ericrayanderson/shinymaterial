5ee838c9c1c6a29f2e8afed03746c3c4f723fa23
ericrayanderson <eric.ray.anderson@gmail.com>
2017-11-21 19:38:07 -0500

    updates

--- R/update-shiny-material-text-box.R
+++ R/update-shiny-material-text-box.R
@@ -1,6 +1,6 @@
-#' Change the value of a material_text_box input on the client
+#' Change the value of a material_text_box on the client
 #'
-#' Change the value of a material_text_box input on the client.
+#' Change the value of a material_text_box on the client.
 #' @param session The session object passed to function given to shinyServer.
 #' @param input_id The input_id of the material_text_box.
 #' @param value The value to set for the material_text_box.
