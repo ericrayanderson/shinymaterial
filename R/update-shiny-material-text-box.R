372d94b27f7821e01bd78244ecbc3e6d6dc570c7
ericrayanderson <eric.ray.anderson@gmail.com>
2017-11-21 19:37:24 -0500

    file update build

--- R/update-shiny-material-text-box.R
+++ R/update-shiny-material-text-box.R
@@ -1,3 +1,15 @@
+#' Change the value of a material_text_box input on the client
+#'
+#' Change the value of a material_text_box input on the client.
+#' @param session The session object passed to function given to shinyServer.
+#' @param input_id The input_id of the material_text_box.
+#' @param value The value to set for the material_text_box.
+#' @examples
+#' update_material_text_box(
+#'   session,
+#'   input_id = "example_text_box",
+#'   value = "New Text"
+#' )
 update_material_text_box <- function(session, input_id, value = NULL){
   if(is.null(value)) {
     return(NULL)
