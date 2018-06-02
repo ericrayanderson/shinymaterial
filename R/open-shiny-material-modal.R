3f6058dd17b2b2a12f818a5d9ae8695bfc805a91
Eric Anderson <ericrayanderson@Erics-MacBook-Pro.local>
2018-06-02 12:09:10 -0400

    v0.5.3

--- R/open-shiny-material-modal.R
+++ R/open-shiny-material-modal.R
@@ -4,7 +4,9 @@
 #' @param session The session object passed to function given to shinyServer.
 #' @param modal_id String. The ID of the modal to open.
 #' @examples
+#' \dontrun{
 #' open_material_modal(session, "example_modal")
+#' }
 open_material_modal <- function(session, modal_id){
   
   js_code <- paste0("$('#", modal_id, "').modal('open');")
--- R/shiny-material-side-nav-tabs-info.R
+++ R/shiny-material-side-nav-tabs-info.R
@@ -3,7 +3,9 @@
 #' Query information about the side-nav tabs (e.g. which tab is active).
 #' @param input The input object in the shiny session.
 #' @examples
+#' \dontrun{
 #' side_nav_tabs_info(input)
+#' }
 side_nav_tabs_info <- function(input = NULL){
   
   if(is.null(input$side_nav_tab_info)){
