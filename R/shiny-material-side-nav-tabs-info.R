148ecb6e328207e819b958833712adcd29d1cdff
Eric Anderson <ericrayanderson@Erics-MacBook-Pro.local>
2018-06-02 11:11:00 -0400

    Tab info docs

--- R/shiny-material-side-nav-tabs-info.R
+++ R/shiny-material-side-nav-tabs-info.R
@@ -1,6 +1,6 @@
 #' Query information about the side-nav tabs.
 #'
-#' Query information about the side-nav tabs.
+#' Query information about the side-nav tabs (e.g. which tab is active).
 #' @param input The input object in the shiny session.
 #' @examples
 #' side_nav_tabs_info(input)
