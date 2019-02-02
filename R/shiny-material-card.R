fa43c38e10fc9dd83044f912a548369438178bae
Eric Anderson <ericrayanderson@Erics-MacBook-Pro.local>
2019-02-02 12:46:14 -0500

    color for card

--- R/shiny-material-card.R
+++ R/shiny-material-card.R
@@ -4,6 +4,7 @@
 #' @param title String. The title of the card
 #' @param ... The UI elements to place in the card
 #' @param depth Integer. The amount of depth of the card. The value should be between 0 and 5. Leave empty for the default depth.
+#' @param color String. The color of the card background. Leave empty for the default color. Visit \url{http://materializecss.com/color.html} for a list of available colors.
 #' @param divider logical. Should there be a divider element between card title and card content?
 #' @examples
 #' material_card(
@@ -11,16 +12,20 @@
 #'   depth = 5,
 #'   shiny::tags$h5("Card Content")
 #' )
-material_card <- function(title, ..., depth = NULL, divider = FALSE){
+material_card <- function(title, ..., depth = NULL, color = NULL, divider = FALSE){
   
   shiny::tags$div(
     class = 
-      paste0(
+      paste(
         "card",
         ifelse(
           is.null(depth),
           "",
-          paste0(" z-depth-", depth)
+          paste0(" z-depth-", depth),
+          ifelse(
+            is.null(color),
+            "",
+            color)
         )
       ),
     shiny::tags$div(
