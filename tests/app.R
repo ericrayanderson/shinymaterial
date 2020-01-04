05aed13c238dbb30a1c6da9cff223ff5c4ca5144
Eric Anderson <ericrayanderson@Erics-MacBook-Pro.local>
2020-01-04 11:03:18 -0500

    include own version of materialize

--- tests/app.R
+++ tests/app.R
@@ -3,7 +3,7 @@ if(interactive()){
   library(shinymaterial)
   
   ui <-
-    material_page(
+    material_page(materialize_in_www = TRUE,
       title = "Testing", nav_bar_fixed = TRUE,
       nav_bar_color = 'red lighten-3',
       background_color = "white",
