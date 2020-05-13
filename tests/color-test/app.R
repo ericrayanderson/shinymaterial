7cb2f101feac4e46bbcf09944550daf384efd48b
Eric Anderson <ericrayanderson@Erics-MacBook-Pro.local>
2020-05-12 22:05:02 -0400

    test dropdown color

--- tests/color-test/app.R
+++ tests/color-test/app.R
@@ -15,7 +15,7 @@ ui <- material_page(
       material_slider("slider", "Slider", min_value = 0, max_value = 2, initial_value = 1),
       material_checkbox(input_id = "checkbox", label = "Checkbox", initial_value = TRUE),
       material_radio_button("radio", "Radio", choices = c("a", "b")),
-      material_dropdown("dropdown", "Dropdown", choices = c("a", "b")),
+      material_dropdown("dropdown", "Dropdown", choices = c("a", "b"), color = "blue"),
       material_date_picker("date", "Date"),
       material_file_input("file", "File"),
       material_floating_button("floating", color = "blue"),
