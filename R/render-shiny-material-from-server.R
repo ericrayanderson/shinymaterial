48a3fe33ed5441980b97c5716d59fb3fbd47848f
Eric Anderson <ericrayanderson@Erics-MacBook-Pro.local>
2020-07-23 22:47:11 -0400

    render material from server

---
+++ R/render-shiny-material-from-server.R
@@ -0,0 +1,27 @@
+#' Render reactive UI shinymaterial elements
+#'
+#' This function is used within a shiny::renderUI. The corresponding output is still referenced using shiny::uiOtuput.
+#'
+#' @param material_ui shinymaterial UI element(s)
+#' 
+#' @examples
+#' ## Only run examples in interactive R sessions
+#' if (interactive()) {
+#'
+#' ui <- material_page(
+#'   uiOutput("renderedButton")
+#' )
+#'
+#' server <- function(input, output) {
+#'   output$renderedButton <- renderUI({
+#'   render_material_from_server(material_button("example_button", "Button"))
+#' })
+#' }
+#' shinyApp(ui, server)
+#' }
+render_material_from_server <- function(material_ui){
+  shiny::tagList(
+    shiny::tags$script("M.AutoInit();"),
+    material_ui
+  )
+}
