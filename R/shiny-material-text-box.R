7ecd3d32a1579dce4a5493fe7d865c9da5a28fe2
Eric Anderson <ericrayanderson@Erics-MacBook-Pro.local>
2020-07-25 12:08:23 -0400

    value for text box

--- R/shiny-material-text-box.R
+++ R/shiny-material-text-box.R
@@ -3,6 +3,7 @@
 #' Build a shinymaterial text box.
 #' @param input_id String. The input identifier used to access the value.
 #' @param label String. The text box label.
+#' @param value String. Initial value.
 #' @param color String. The accent color of the text box. Leave empty for the default color. Visit \url{http://materializecss.com/color.html} for a list of available colors. \emph{This input requires using color hex codes, rather than the word form. E.g., "#ef5350", rather than "red lighten-1".}
 #' @param icon String. The name of the icon. Leave empty for no icon. Visit \url{http://materializecss.com/icons.html} for a list of available icons.
 #' @seealso \code{\link{update_material_text_box}}
@@ -13,7 +14,7 @@
 #'   icon = "search",
 #'   color = "#ef5350"
 #' )
-material_text_box <- function(input_id, label, color = NULL, icon = NULL){
+material_text_box <- function(input_id, label, value = "", color = NULL, icon = NULL){
   if (!is.null(color)) {
 
     if (!is.null(icon)) {
@@ -68,7 +69,8 @@ material_text_box <- function(input_id, label, color = NULL, icon = NULL){
           shiny::tags$input(
             id = input_id,
             type = "text",
-            class = "validate"
+            class = "validate",
+            value = value
           ),
           shiny::tags$label(
             `for` = input_id,
